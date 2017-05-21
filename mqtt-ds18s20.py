84b78dfaf4ab07162d4de5621d4b0704c8077cc4
Ricardo Salveti <rsalveti@rsalveti.net>
2017-05-21 19:14:36 -0300

    mqtt-ds18s20: migrate to utils.config and umqtt robust
    Signed-off-by: Ricardo Salveti <rsalveti@rsalveti.net>

--- mqtt-ds18s20.py
+++ mqtt-ds18s20.py
@@ -2,67 +2,48 @@ import time
 import machine
 import onewire, ds18x20
 import ubinascii
-from umqtt.simple import MQTTClient
-
-# Defaults are overwritten with the contents of /config.json by load_config()
-CONFIG = {
-    "broker": "192.168.1.30",
-    "port": 1883,
-    "user": "foo",
-    "password": "bar",
-    "gpio": 12,
-    "topic": "brew/temperature",
-}
-
-def load_config():
-    import ujson as json
-    try:
-        with open("/config.json") as f:
-            config = json.loads(f.read())
-    except (OSError, ValueError):
-        print("Couldn't load /config.json, saving defaults")
-        save_config()
-    else:
-        CONFIG.update(config)
-        print("Loaded config from /config.json")
-
-def save_config():
-    import ujson as json
-    try:
-        with open("/config.json", "w") as f:
-            f.write(json.dumps(CONFIG))
-    except OSError:
-        print("Couldn't save /config.json")
+import utils
+from umqtt.robust import MQTTClient
 
 def main():
-    client_id = "esp8266_" + ubinascii.hexlify(machine.unique_id()).format()
-    client = MQTTClient(client_id, CONFIG['broker'], CONFIG['port'],
-                        CONFIG['user'], CONFIG['password'])
-    client.connect()
-    print("Connected to {}".format(CONFIG['broker']))
+    config = utils.Config()
 
-    ds_data = machine.Pin(CONFIG['gpio'])
+    ds_data = machine.Pin(config.get("w1_gpio"), 14)
     ds = ds18x20.DS18X20(onewire.OneWire(ds_data))
 
     # Scan for devices on the bus
     roms = ds.scan()
     if roms:
-        print("Found DS devices:",
+        print("Found DS18S20 sensors:",
                 "".join("%s " % ubinascii.hexlify(b).format() for b in roms))
     else:
-        print("No DS sensor found")
+        print("No DS18S20 sensor found, nothing to be reported")
+        return
+
+    client_id = "esp8266_" + ubinascii.hexlify(machine.unique_id()).format()
+    client = MQTTClient(client_id, config.get("mqtt_broker"),
+                        config.get("mqtt_port"), config.get("mqtt_user"),
+                        config.get("mqtt_passwd"))
+    try:
+        client.connect()
+    except OSError as e:
+        # Just report and continue, since publish will try to reconnect
+        print("Error when connecting to the MQTT broker")
+    else:
+        print("Connected to {}".format(config.get("mqtt_broker")))
 
     # Iterate and publish
     while True:
         ds.convert_temp()
         time.sleep_ms(750)
         for rom in roms:
-            client.publish("{}/{}".format(CONFIG['topic'],
-                                          ubinascii.hexlify(rom).format()),
+            ds_id = ubinascii.hexlify(rom).format()
+            ds_topic = "w1_" + ds_id
+            client.publish("{}/{}/temperature".format(config.get("mqtt_topic"),
+                                          config.get(ds_topic, ds_id)),
                                           bytes(str(ds.read_temp(rom)),
                                           'utf-8'))
         time.sleep(5)
 
 if __name__ == '__main__':
-    load_config()
     main()

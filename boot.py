afedc31aed53ce80aef71e756e09b41e8b605228
Ricardo Salveti <rsalveti@rsalveti.net>
2017-05-21 18:04:09 -0300

    boot.py: use the new config class for wifi credentials
    Signed-off-by: Ricardo Salveti <rsalveti@rsalveti.net>

--- boot.py
+++ boot.py
@@ -1,18 +1,19 @@
 def do_connect():
     import network
+    import utils
 
-    SSID = "foo"
-    PASSWORD = "foobar"
-
+    config = utils.Config()
     sta_if = network.WLAN(network.STA_IF)
     ap_if = network.WLAN(network.AP_IF)
+
     if ap_if.active():
         print("Disabling AP...");
         ap_if.active(False)
     if not sta_if.isconnected():
         print("Connecting to the network...")
         sta_if.active(True)
-        sta_if.connect(SSID, PASSWORD)
+        sta_if.connect(config.get("wifi_ssid"),
+                        config.get("wifi_passwd"))
         while not sta_if.isconnected():
             pass
     print("Network configuration: ", sta_if.ifconfig())
